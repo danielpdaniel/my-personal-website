e1caaa92742b250fd32470e6efcefbdc08323af7
Jim Quinn <stapledpencil@gmail.com>
2022-08-02 09:27:04 -0400

    style test changes (ugly)

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ document.querySelector("body").appendChild(h2);
 
 
 document.getElementById("bailey").style.cursor = "pointer";
+
+const baileyGirl = document.getElementById("bailey");
+
+function clickAlert() {
+    alert('I was clicked!');
+}
+
+function addClickAlert () {
+   baileyGirl.addEventListener('click', clickAlert);
+}
+
+addClickAlert();
+
